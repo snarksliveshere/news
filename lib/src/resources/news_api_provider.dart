f237776e2fb987ba65d32d191ec624a19f9e4b5b
snarksliveshere <snarksliveshere@gmail.com>
2019-03-31 19:11:31 +0300

    working with DB

--- lib/src/resources/news_api_provider.dart
+++ lib/src/resources/news_api_provider.dart
@@ -1,21 +1,22 @@
 import 'package:http/http.dart' show Client;
 import 'dart:convert';
 import '../models/item_model.dart';
+import 'dart:async';
 
 final _root = 'https://hacker-news.firebaseio.com/v0';
 
 class NewsApiProvider {
   Client client = Client();
 
-  fetchTopIds() async {
+  Future<List<int>> fetchTopIds() async {
     var url = '$_root/topstories.json';
     final response = await client.get(url);
     final ids = json.decode(response.body);
 
-    return ids;
+    return ids.cast<int>();
   }
 
-  fetchItem(int id) async {
+  Future<ItemModel> fetchItem(int id) async {
     var url = '$_root/item/$id.json';
     final response = await client.get(url);
     final parsedJson = json.decode(response.body);
