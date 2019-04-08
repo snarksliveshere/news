acdbdbce048c1061ef5dad87225a8d415d3db45b
snarksliveshere <snarksliveshere@gmail.com>
2019-04-08 18:17:14 +0300

    transformer comments

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,16 +2,19 @@ import 'package:flutter/material.dart';
 import 'screens/news_list.dart';
 import 'blocs/stories_provider.dart';
 import 'screens/news_detail.dart';
+import 'blocs/comments_provider.dart';
 
 
 class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StoriesProvider(
-      child: MaterialApp(
-        title: 'News!',
-        onGenerateRoute: this.routes,
+    return CommentsProvider(
+      child: StoriesProvider(
+          child: MaterialApp(
+            title: 'News!',
+            onGenerateRoute: this.routes,
+          )
       )
     );
   }
@@ -26,8 +29,13 @@ class App extends StatelessWidget {
     } else {
       return MaterialPageRoute(
         builder: (context) {
+          final commentsBloc = CommentsProvider.of(context);
+          final itemId = int.parse(settings.name.replaceFirst('/', ''));
+          commentsBloc.fetchItemWithComments(itemId);
           // extract item.id from settings.name
-          return NewsDetail();
+          return NewsDetail(
+            itemId: itemId
+          );
         }
       );
     }
--- lib/src/screens/news_detail.dart
+++ lib/src/screens/news_detail.dart
@@ -1,9 +1,42 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import '../blocs/comments_provider.dart';
+import '../models/item_model.dart';
 
 class NewsDetail extends StatelessWidget {
+  final int itemId;
 
+  NewsDetail({this.itemId});
   @override
   Widget build(BuildContext context) {
-    return Text('news detail');
+    final bloc = CommentsProvider.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Detail')
+      ),
+      body: buildBody(bloc)
+    );
+  }
+
+  Widget buildBody(CommentsBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.itemWithComments,
+      builder: (context, AsyncSnapshot<Map<int,Future<ItemModel>>> snapshot) {
+        if (!snapshot.hasData) {
+          return Text('Loading1');
+        }
+        final itemFuture = snapshot.data[itemId];
+        return FutureBuilder(
+          future: itemFuture,
+          builder: (contexts, AsyncSnapshot<ItemModel> itemSnapshot) {
+            if (!itemSnapshot.hasData) {
+              return Text('Loading2');
+            }
+
+            return Text(itemSnapshot.data.title);
+          },
+        );
+      },
+    );
   }
 }
